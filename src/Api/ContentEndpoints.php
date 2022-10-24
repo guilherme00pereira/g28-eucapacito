7c1f71f705514d7500526c4c272966aa7a22ad92
Guilherme Pereira <guilherme00pereira@gmail.com>
2022-10-24 15:16:19 -0300

    v_0.4.13

---
+++ src/Api/ContentEndpoints.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace G28\Eucapacito\Api;
+
+use WP_REST_Response;
+use G28\Eucapacito\Core\DBQueries;
+
+class ContentEndpoints
+{
+
+    protected static ?ContentEndpoints $_instance = null;
+
+    public function __construct()
+    {
+    }
+
+    public static function getInstance(): ?ContentEndpoints {
+        if ( is_null( self::$_instance ) ) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    public static function getAllPostsSlug(): WP_REST_Response
+    {
+        $slugs = DBQueries::getPostsSlug();
+        return new WP_REST_Response( $slugs , 200 );
+    }
+
+    public static function getAllScholarshipsSlug(): WP_REST_Response
+    {
+        $slugs = DBQueries::getPostsSlug();
+        return new WP_REST_Response( $slugs , 200 );
+    }
+
+    public static function getAllEmployabilitiesSlug(): WP_REST_Response
+    {
+        $slugs = DBQueries::getEmployabilitiesSlug();
+        return new WP_REST_Response( $slugs , 200 );
+    }
+
+    public static function getAllJourneysSlug(): WP_REST_Response
+    {
+        $slugs = DBQueries::getJourneysSlug();
+        return new WP_REST_Response( $slugs , 200 );
+    }
+    
+}
